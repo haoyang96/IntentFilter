e6796dc7f63646658e777b943f374873b819deba
unknown <>
2016-09-07 08:22:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/intentfilter/AnotherActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentfilter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 杨浩 on 2016/9/6.
+ */
+public class AnotherActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
